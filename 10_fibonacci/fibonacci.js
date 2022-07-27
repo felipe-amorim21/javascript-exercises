3bfd0f760a465af36bc488debf24fbe68e37b87d
Felipe Amorim <92737458+felipe-amorim21@users.noreply.github.com>
2022-07-27 21:10:38 +0000

    Finished exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
-
-};
-
+const fibonacci = function(number) {
+    let n1 = 1;
+    let n2 = 1;
+    let n3 = 0
+    if(number >2){
+        for(let i = 2;i<number;i++){
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+        }
+        return n3;
+    } else if(number <=0){
+        return "OOPS!"
+    } else {
+        return n1;
+        
+    }
+    
+}
 // Do not edit below this line
 module.exports = fibonacci;
