4085c19a043cb2d91ca032955e52cb0558c6a83c
Felipe Amorim <92737458+felipe-amorim21@users.noreply.github.com>
2022-07-27 20:22:26 +0000

    Finished exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.toLowerCase().replace(/[^a-z]/g, "")
+    newString = Array.from(string)
+    if (string === newString.reverse().join("")){
+        console.log(string)
+        console.log(newString.reverse().join(""))
+        return true
+    } else {
+        console.log(string)
+        console.log(newString)
+        return false
+    }
 };
 
 // Do not edit below this line
