15fc7b349f431f932be38e5f59a1608e5b38b47d
Felipe Amorim <92737458+felipe-amorim21@users.noreply.github.com>
2022-07-29 23:18:46 +0000

    Finished exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest,person) =>{
+        if(!(person.yearOfDeath)){
+            person.yearOfDeath = new Date().getFullYear();
+        }
+        if(!(oldest.yearOfDeath)){
+            oldest.yearOfDeath = new Date().getFullYear();
+        }
+        const personAge = person.yearOfDeath - person.yearOfBirth
+        const oldestAge = oldest.yearOfDeath - oldest.yearOfBirth
+        if(personAge >= oldestAge){
+            return person
+        } else {
+            return oldest
+        }
+    })
 };
 
 // Do not edit below this line
